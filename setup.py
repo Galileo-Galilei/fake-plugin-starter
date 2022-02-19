991c3d51aac497744c4a9553b19254fb485337a3
Yolan Honoré-Rougé <yolan.honore.rouge@gmail.com>
2022-02-19 21:40:14 +0100

    fix typo in description

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ HERE = pathlib.Path(__file__).parent
 setup(
     name=NAME,
     version="0.0.1",
-    description="A fake kedro-plugin to test starter injection functionnality",
+    description="A fake kedro plugin to test starter injection functionnality",
     url="https://github.com/Galileo-Galilei/fake_plugin_starter",
     python_requires=">=3.6, <3.9",
     packages=find_packages(),
